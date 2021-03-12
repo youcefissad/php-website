f55fe51b27835c9f21b0ba091b4a018698c80757
Youcef Issad <yissad1@bcst01.parkland.edu>
2021-03-12 17:12:05 -0600

    adding functions

---
+++ library/functions.php
@@ -0,0 +1,31 @@
+
+<?php
+function head ()
+  {
+  echo "<center>";
+  echo "<h3>Youcef Issad 155201F</h3>";
+  echo "<img src='http://www.csit.parkland.edu/~yissad1/images/1.png'>";
+  }
+?>
+
+<?php
+function footer()
+{
+echo "<center><strong>";
+echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/site.php'> log in page </a>";
+echo "   |  ";
+echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/logout.php'> log out page </a>";
+echo "   |  ";
+echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/welcome.php'> welcome page </a>";
+echo "   |  ";
+echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/purchaseItem1.php'> purchase item 1 page </a>";
+echo "   |  ";
+echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/purchaseItem2.php'> purchase item 2 page </a>";
+echo "   |  ";
+echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/purchaseItem3.php'> purchase item 3 page </a>";
+echo "   |  ";
+echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/purchaseItem4.php'> purchase item 4 page </a>";
+echo "   |  ";
+echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/shoppingCarte.php'> shopping carte page </a>";
+}
+?>
