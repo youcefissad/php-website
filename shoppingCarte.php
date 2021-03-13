d9a4caf6a9479ae134525f97b9292766e240250c
Youcef Issad <yissad1@bcst01.parkland.edu>
2021-03-12 23:54:54 -0600

    edit

--- shoppingCarte.php
+++ shoppingCarte.php
@@ -1,22 +1,33 @@
-<!--  I honor Parkland's core values by affirming that I have
-followed all academic integrity guidelines for this work.
-Youcef ISSAD<!--  I honor Parkland's core values by affirming that I have
+<!--  I honor Parkland's core values by affirming that I have 
 followed all academic integrity guidelines for this work.
 Youcef ISSAD
 csc155201F -->
 <html>
 <head>
-<title>shopping carte</title>
+<title>purchase item 1</title>
 <?php
-// php library loading first
-require("library/functions.php")
+require("library/functions.php");
+secure_pages();
 
 
 ?>
 </head>
 <body>
 <?php  head()?><br><br>
-<p> This will be the shopping carte  page. </p>
-<?php footer() ?>
+<h1> checkout </h1> 
+
+
+<p>  <?php echo print_cups( $_SESSION['cups']);?></p>
+<p> Total Cups= <?php echo $_SESSION['cups'];?> </p>  
+
+<p>  <?php echo print_forks( $_SESSION['forks']);?>  </p>
+<p> Total Forks= <?php echo $_SESSION['forks'];?> </p>
+
+<p>  <?php echo print_knives( $_SESSION['knives']);?>  </p>
+<p> Total Knives= <?php echo $_SESSION['knives'];?> </p>
+
+<p>  <?php echo print_spoons( $_SESSION['spoons']);?>  </p>
+<p> Total Spoons= <?php echo $_SESSION['spoons'];?> </p>
+<?php footer()?>
 </body>
 </html>
