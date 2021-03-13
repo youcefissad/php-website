5c0ad5d4cec2be7b44b7015931ed60dca964c059
Youcef Issad <yissad1@bcst01.parkland.edu>
2021-03-12 23:22:59 -0600

    edit

--- library/functions.php
+++ library/functions.php
@@ -6,26 +6,79 @@ function head ()
   echo "<h3>Youcef Issad 155201F</h3>";
   echo "<img src='http://www.csit.parkland.edu/~yissad1/images/1.png'>";
   }
-?>
+ 
 
-<?php
 function footer()
 {
 echo "<center><strong>";
-echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/site.php'> log in page </a>";
+echo "<a href='site.php'> log in page </a>";
 echo "   |  ";
-echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/logout.php'> log out page </a>";
+echo "<a href='logout.php'> log out page </a>";
 echo "   |  ";
-echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/welcome.php'> welcome page </a>";
+echo "<a href='welcome.php'> welcome page </a>";
 echo "   |  ";
-echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/purchaseItem1.php'> purchase item 1 page </a>";
+echo "<a href='purchaseItem1.php'> purchase cups </a>";
 echo "   |  ";
-echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/purchaseItem2.php'> purchase item 2 page </a>";
+echo "<a href='purchaseItem2.php'> purchase forks </a>";
 echo "   |  ";
-echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/purchaseItem3.php'> purchase item 3 page </a>";
+echo "<a href='purchaseItem3.php'> purchase knives  </a>";
 echo "   |  ";
-echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/purchaseItem4.php'> purchase item 4 page </a>";
+echo "<a href='purchaseItem4.php'> purchase spoons </a>";
 echo "   |  ";
-echo "<a href='http://www.csit.parkland.edu/~yissad1/CSC/csc155201F/12-PHPsite/shoppingCarte.php'> shopping carte page </a>";
+echo "<a href='shoppingCarte.php'> shopping carte page </a>";
 }
+
+function secure_pages()
+{
+session_start();
+
+       if ( !isset( $_SESSION['user'] ) )
+      
+           {
+           header("Location: site.php");
+           }
+}
+
+function getPost($name)
+{
+
+ if (isset($_POST[$name]) )
+    {
+        return htmlspecialchars($_POST[$name]);
+    }
+    return "";
+}
+
+function print_cups($times)
+{
+  for ($i=0;$i<$times;$i++)
+  {
+  echo "C";
+  }
+}
+
+function print_forks($times)
+{
+  for ($i=0;$i<$times;$i++)
+  {
+  echo "F";
+  }
+}
+
+function print_knives($times)
+{
+  for ($i=0;$i<$times;$i++)
+  {
+  echo "K";
+  }
+}
+
+function print_spoons($times)
+{
+  for ($i=0;$i<$times;$i++)
+  {
+  echo "S";
+  }
+}
+
 ?>
