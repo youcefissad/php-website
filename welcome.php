31f75a8b73b54e11da24d30d25d8ddcf7b8f525b
Youcef Issad <yissad1@bcst01.parkland.edu>
2021-03-27 17:16:21 -0500

    main

--- welcome.php
+++ welcome.php
@@ -10,15 +10,29 @@ csc155201F -->
 // php library loading first
 require("library/functions.php");
 secure_pages();
+if (isset($_POST['submit']))
+   {
+    setcookie("name",$_POST['name'], time() + (86400 * 30));
+   }
 
- 
+
 ?>
 </head>
 <body>
 <?php head()?><br><br>
 <p> welcome page is here </p>
+<form method='POST'>
+<table border='1'>
+<tr>
+<td>first and last name</td>
+<td><input type='text' name='name' value=''></td>
+</tr>
+<tr >
+<td colspan='3'><input type='submit' name='submit' value='enter first and last name'>
+</td></tr>
+</table>
+</form>
 <?php footer()?>
-<?php echo $_SESSION['user'];?>
 </body>
 </html>
 
