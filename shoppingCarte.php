936d36670156c5bcbf0e2f3db8843976b74ae140
Youcef Issad <yissad1@bcst01.parkland.edu>
2021-04-24 12:50:40 -0500

    addingStuff

--- shoppingCarte.php
+++ shoppingCarte.php
@@ -9,6 +9,41 @@ csc155201F -->
 require("library/functions.php");
 secure_pages();
 
+function insertNewRecord($conn)
+{
+   // prepare and bind
+    $stmt = $conn->prepare("INSERT INTO user_orders (username, date, cups, forks, spoon, knives) VALUES (?,?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssss", $username, $date, $cups, $forks, $spoons, $knives);
+    
+    // set parameters and execute
+    $username = getPost('username2021');
+    $date = $_POST['date'];
+    $cups = $_SESSION['cups'];
+    $forks = $_SESSION['forks'];
+    $spoons = $_SESSION['spoons'];
+    $knives = $_SESSION['knives'];
+
+    $stmt->execute(); 
+}
+
+
+// Create connection object
+$user = "yissad1";  
+$conn = mysqli_connect("localhost",$user,$user,$user);
+
+
+
+if (isset($_POST['submit']))
+{
+    if (($_POST['submit'] == "buy one")||($_POST['submit'] == "buy five")||($_POST['submit'] == "buy twenty five"))
+    {
+    if (!empty( $_POST['name'] )) 
+    {
+        insertNewRecord($conn);
+    }
+    }
+}
+
 
 
 ?>
